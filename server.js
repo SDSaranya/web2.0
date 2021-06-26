a069988215043df20f71bb9e628bacec5403230b
saranya <saranya.d.srinivasan@gmail.com>
2021-06-26 19:03:27 +0530

    Added Validation mesage

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res)=> {
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
+
+app.post('/', (req, res)=> {
     var email = req.body.email;
     var amount = req.body.amount;
+    if(amount <= 1) {
+       return_info ={};
+       return_info.error = true;
+       return_info.message = 'Amount should be greater than 1';
+       res.send(return_info);
+    }
     res.send({"amount": amount, "email": email});
 });
 
 app.listen(3000, () => {
-console.log('server running on port 30000')
+console.log('server running on port 3000')
 });
